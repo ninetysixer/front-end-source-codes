5c4505ca8280d8b551803a24419085f6b65752ef
Semih Dursungül <114025283+semihdursungul@users.noreply.github.com>
2023-05-19 16:33:28 +0300

    Create script.js

---
+++ clock/script.js
@@ -0,0 +1,18 @@
+function updateTime() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  var timeString = hours + ":" + minutes + ":" + seconds;
+
+  document.getElementById("time").textContent = timeString;
+}
+
+updateTime();
+
+setInterval(updateTime, 1);
