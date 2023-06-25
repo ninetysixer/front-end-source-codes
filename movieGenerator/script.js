7f6ebbf3c0174a4370508f72eb86c85810061fb7
Semih Dursungül <114025283+semihdursungul@users.noreply.github.com>
2023-06-25 12:14:29 +0300

    Create script.js

---
+++ movieGenerator/script.js
@@ -0,0 +1,56 @@
+const apiKey = 'YOUR-API-KEY';
+
+function getRandomMovie() {
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+  
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const randomIndex = Math.floor(Math.random() * data.results.length);
+      const movie = data.results[randomIndex];
+      displayMovieDetails(movie);
+    })
+    .catch(error => {
+      console.error('Error fetching random movie:', error);
+    });
+}
+
+function displayMovieDetails(movie) {
+  const titleElement = document.getElementById('movie-title');
+  const posterElement = document.getElementById('movie-poster');
+  const overviewElement = document.getElementById('movie-overview');
+  const genresElement = document.getElementById('movie-genres');
+  const imdbElement = document.getElementById('movie-imdb');
+
+  titleElement.textContent = movie.title;
+  posterElement.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+  overviewElement.textContent = movie.overview;
+  genresElement.textContent = `Genres: ${getGenresString(movie.genre_ids)}`;
+
+  // Fetch IMDb details using another API or calculate IMDb score based on available data
+  const imdbScore = calculateImdbScore(movie.vote_average);
+  imdbElement.textContent = `IMDb Score: ${imdbScore}`;
+}
+
+function getGenresString(genreIds) {
+  // Map genre IDs to their respective names (you may need to fetch genre data from TMDb API)
+  const genreNames = {
+    28: 'Action',
+    35: 'Comedy',
+    18: 'Drama',
+    // Add more genre mappings as needed
+  };
+
+  const genres = genreIds.map(id => genreNames[id] || '').filter(Boolean);
+  return genres.join(', ');
+}
+
+function calculateImdbScore(voteAverage) {
+  // Calculate IMDb score based on the vote average
+  // You can implement your own formula here
+  const imdbScore = voteAverage / 2;
+  return imdbScore.toFixed(1);
+}
+
+document.addEventListener('DOMContentLoaded', getRandomMovie);
+document.getElementById('get-movie-btn').addEventListener('click', getRandomMovie);
